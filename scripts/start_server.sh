d2653a662053e55cb43ed876e319b1cfbad44357
Kundan547 <kundanvyas197@gmail.com>
2024-10-25 00:01:28 +0530

    Bug is fixed

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,9 +3,5 @@
 cd /home/ubuntu/CalmNest
 # Activate the virtual environment
 source venv/bin/activate
-<<<<<<< HEAD
-# Start the Flask app using Gunicorn
-=======
 # Start the Flask app with Gunicorn for production
->>>>>>> 12873b4d42bd5d1389b520fcd1f0aa3ad535ea07
 gunicorn --bind 0.0.0.0:5000 app:app &
