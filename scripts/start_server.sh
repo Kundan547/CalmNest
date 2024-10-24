12873b4d42bd5d1389b520fcd1f0aa3ad535ea07
Ubuntu <ubuntu@ip-172-31-20-18.us-east-2.compute.internal>
2024-10-24 09:28:27 +0000

    Testing

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Navigate to the application directory
-cd /home/ec2-user/CalmNest
+cd /home/ubuntu/CalmNest
+# Activate the virtual environment
+source venv/bin/activate
 # Start the Flask app with Gunicorn for production
 gunicorn --bind 0.0.0.0:5000 app:app &
 
--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Add a delay to ensure the application has time to start
+sleep 5
+
 # Check if the application is running
 if pgrep -f "gunicorn" > /dev/null; then
     echo "Application is running."
@@ -7,4 +10,5 @@ else
     echo "Application is not running!"
     exit 1
 fi
+
 
